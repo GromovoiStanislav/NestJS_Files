13375db75b7edf060bb25f5a74fe4f8160bac1d1
Gromovoi Stanislav <gromila12@rambler.ru>
2023-04-06 15:13:07 +0600

    resize

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -18,6 +18,7 @@ import { join } from "node:path";
 import { Response } from "express";
 
 
+
 @Controller("files")
 export class FilesController {
 
@@ -31,14 +32,72 @@ export class FilesController {
     const files: MFile[] = [new MFile(file)];
 
     if (file.mimetype.includes("image")) {
-      const webP = await this.filesService.convertToWebp(file.buffer);
+      let webP = await this.filesService.convertToWebp(file.buffer);
+      const webP200 = await this.filesService.resizeTo(webP, 200);
+
       files.push(new MFile({
         originalname: `${file.originalname.split(".")[0]}.webp`,
         buffer: webP
       }));
+
+      files.push(new MFile({
+        originalname: `${file.originalname.split(".")[0]}_200x200.webp`,
+        buffer: webP200
+      }));
+
     }
 
     return this.filesService.saveFiles(files);
+  }
+
+
+  @Post("upload2")
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadFileAndResize2(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
+
+    const files: MFile[] = [new MFile(file)];
+    const sizes = ["25X25", "50X50", "50X50", "200X200", "400X400", "900X900"];
+    const [, ext] = file.mimetype.split("/");
+
+    if (["jpeg", "jpg", "png"].includes(ext)) {
+
+      // for (const s of sizes) {
+      //   const [size] = s.split("X");
+      //   const resisedFile = await this.filesService.resizeTo(file.buffer, Number(size));
+      //   files.push(new MFile({
+      //     originalname: `${file.originalname.split(".")[0]}_${s}.${ext}`,
+      //     buffer: resisedFile
+      //   }));
+      // }
+
+      await Promise.all(sizes.map(async (s) => {
+        const [size] = s.split("X");
+        const resisedFile = await this.filesService.resizeTo(file.buffer, Number(size));
+        files.push(new MFile({
+          originalname: `${file.originalname.split(".")[0]}_${s}.${ext}`,
+          buffer: resisedFile
+        }));
+      }));
+
+    }
+
+    return this.filesService.saveFiles(files);
+  }
+
+
+  @Post("upload3")
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadFileAndResize3(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
+
+    let res: FileElementResponse[] = [];
+
+    const [, ext] = file.mimetype.split("/");
+    if (["jpeg", "jpg", "png"].includes(ext)) {
+      res = await this.filesService.ResizeAndSaveFile(file.buffer, ext);
+    }
+
+    return res
+
   }
 
 
@@ -47,7 +106,7 @@ export class FilesController {
           @Param("filename") filename: string,
           @Res({ passthrough: true }) res: Response): StreamableFile {
 
-    const file = createReadStream(join(process.cwd(), "uploads",dirname, filename));
+    const file = createReadStream(join(process.cwd(), "uploads", dirname, filename));
 
     const fileExtension: string = filename.split(".").pop();
     //const fileExtension: string = path.parse(filename).ext;
@@ -63,10 +122,10 @@ export class FilesController {
 
 
   @Get("download")
-  @Header('Content-Type', 'application/json')
-  @Header('Content-Disposition', 'attachment; filename="package.json"')
+  @Header("Content-Type", "application/json")
+  @Header("Content-Disposition", "attachment; filename=\"package.json\"")
   getPackageJson(): StreamableFile {
-    const file = createReadStream(join(process.cwd(), 'package.json'));
+    const file = createReadStream(join(process.cwd(), "package.json"));
     return new StreamableFile(file);
   }
 
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -5,28 +5,54 @@ import { join } from "node:path";
 import { ensureDir, writeFile } from "fs-extra";
 import * as sharp from "sharp";
 import { MFile } from "./dto/mfile.class";
+import { v4 } from "uuid";
+
 
 @Injectable()
 export class FilesService {
 
   async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
     const dateFolder = format(new Date(), "yyyy-MM-dd");
-    const uploadFolder = join(process.cwd(),'uploads',dateFolder);
+    const uploadFolder = join(process.cwd(), "uploads", dateFolder);
     await ensureDir(uploadFolder);
 
     const res: FileElementResponse[] = [];
 
     for (const file of files) {
-      await writeFile(join(uploadFolder,file.originalname), file.buffer);
+      await writeFile(join(uploadFolder, file.originalname), file.buffer);
       res.push({ url: `${dateFolder}/${file.originalname}`, name: file.originalname });
     }
 
     return res;
   }
 
-  convertToWebp(file: Buffer): Promise<Buffer> {
+  async convertToWebp(file: Buffer): Promise<Buffer> {
     return sharp(file).webp().toBuffer();
   }
 
+  async resizeTo(file: Buffer, size: number): Promise<Buffer> {
+    return sharp(file).resize(size, size).toBuffer();
+  }
 
+  async ResizeAndSaveFile(file: Buffer, ext: string): Promise<FileElementResponse[]> {
+    const sizes = ["25X25", "50X50", "50X50", "200X200", "400X400", "900X900"];
+    const filename = `${v4()}.${ext}`;
+    const uploadFolder = join(process.cwd(), "uploads");
+
+    const res: FileElementResponse[] = [];
+
+    for (const s of sizes) {
+      await ensureDir(`${uploadFolder}/${s}`);
+      const [size] = s.split("X");
+      await sharp(file)
+        .resize(+size, +size)
+        .toFile(
+          `${uploadFolder}/${s}/${filename}`
+          //`${__dirname}/../../public/uploads/${s}/${filename}`,
+        );
+      res.push({ url: `${s}/${filename}`, name: filename });
+    }
+    return res;
+
+  }
 }
